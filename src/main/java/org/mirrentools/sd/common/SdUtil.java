b7d182ada83f4f4fdcfceb130b686ad71ef754b8
690389491@qq.com <miniand3>
2019-01-31 00:21:29 +0800

    完成分词

--- src/main/java/org/mirrentools/sd/common/SdUtil.java
+++ src/main/java/org/mirrentools/sd/common/SdUtil.java
@@ -1,10 +1,7 @@
 package org.mirrentools.sd.common;
 
-import static org.hamcrest.CoreMatchers.both;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,8 +11,12 @@ import java.util.List;
  *
  */
 public class SdUtil {
-	/** 指定字符串替换为下划线的默认值 */
-	private static final String[] DEFAULT_REPLACE_REPS = {" ", "-"};
+	/** 默认分词符号 */
+	public final static String DEFAULT_PARTICIPLE_FLAG = "<--->";
+	/** 分词中默认要移除的字符串 */
+	public final static List<String> DEFAULT_PARTICIPLE_EXCLUDES = asList(" ");
+	/** 分词中默认要转换为连接词的字符串 */
+	public final static List<String> DEFAULT_PARTICIPLE_TO_JOIN = asList("-", "_");
 
 	/**
 	 * 获取用户项目根目录
@@ -33,19 +34,25 @@ public class SdUtil {
 	 * @return
 	 */
 	public static String toHyphenCase(String str) {
-
-		return null;
+		String underScore = splitJoin(str, DEFAULT_PARTICIPLE_FLAG);
+		String[] split = underScore.split(DEFAULT_PARTICIPLE_FLAG);
+		StringBuilder sb = new StringBuilder(split[0]);
+		for (int i = 1; i < split.length; i++) {
+			sb.append("-" + split[i]);
+		}
+		return sb.toString().replaceAll("-+", "-");
 	}
+
 	/**
 	 * 将字符串转换为连字符命名
 	 * 
 	 * @param str
 	 * @return
 	 */
-	public static String toUpperHyphenCase(String str) {
-
+	public static String toHyphenCaseUpper(String str) {
 		return toHyphenCase(str).toUpperCase();
 	}
+
 	/**
 	 * 将字符串转换为下划线命名
 	 * 
@@ -53,16 +60,22 @@ public class SdUtil {
 	 * @return
 	 */
 	public static String toUnderScoreCase(String str) {
-
-		return null;
+		String underScore = splitJoin(str, DEFAULT_PARTICIPLE_FLAG);
+		String[] split = underScore.split(DEFAULT_PARTICIPLE_FLAG);
+		StringBuilder sb = new StringBuilder(split[0]);
+		for (int i = 1; i < split.length; i++) {
+			sb.append("_" + split[i]);
+		}
+		return sb.toString().replaceAll("_+", "_");
 	}
+
 	/**
 	 * 将字符串转换为下划线命名
 	 * 
 	 * @param str
 	 * @return
 	 */
-	public static String toUpperUnderScoreCase(String str) {
+	public static String toUnderScoreCaseUpper(String str) {
 		return toUnderScoreCase(str).toUpperCase();
 	}
 
@@ -73,8 +86,14 @@ public class SdUtil {
 	 * @return
 	 */
 	public static String toPascalCase(String str) {
+		String underScore = splitJoin(str, DEFAULT_PARTICIPLE_FLAG);
+		String[] split = underScore.split(DEFAULT_PARTICIPLE_FLAG);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < split.length; i++) {
+			sb.append(firstToUpperCase(split[i]));
+		}
+		return sb.toString();
 
-		return str;
 	}
 
 	/**
@@ -84,8 +103,8 @@ public class SdUtil {
 	 * @return
 	 */
 	public static String toCamelCase(String str) {
-		String underScore = replaceToUnderScore(str, DEFAULT_REPLACE_REPS);
-		String[] split = underScore.split("_");
+		String underScore = splitJoin(str, DEFAULT_PARTICIPLE_FLAG);
+		String[] split = underScore.split(DEFAULT_PARTICIPLE_FLAG);
 		StringBuilder sb = new StringBuilder(split[0]);
 		for (int i = 1; i < split.length; i++) {
 			sb.append(firstToUpperCase(split[i]));
@@ -128,89 +147,114 @@ public class SdUtil {
 	}
 
 	/**
-	 * 将字符串替换为使用下划线分割词语并将词汇小写,如果字符串以_开头则将其删除
+	 * 将字符串大小写字母按词拆分,并将其连接成新的字符串
 	 * 
 	 * @param str
-	 * @param reps
-	 *          指定将什么字符替换为下划线
+	 *          字符串
+	 * @param join
+	 *          连接词,如果字符串存在loin则按join分词并返回
 	 * @return
 	 */
-	public static String replaceToUnderScore(String str, String... reps) {
-		for (String r : reps) {
-			str = str.replace(r, "_");
-		}
-		StringBuilder sb = new StringBuilder();
-		int i = 0;
-		while (i < str.length()) {
-			if (isLetters(str.charAt(i))) {
-				break;
-			}
-			if ('_' != str.charAt(i)) {
-				sb.append(str.charAt(i));
-			}
-			i++;
-		}
-		boolean first = true;
-		for (; i < str.length(); i++) {
-			char c = str.charAt(i);
-			if (!first && (isUpper(c) != isUpper(str.charAt(i - 1)) || isLower(c) != isLower(str.charAt(i - 1)))) {
-				sb.append("_");
-				sb.append(c);
-			} else {
-				sb.append(c);
-			}
-			first = false;
-		}
-		String temp = sb.toString().replaceAll("_+", "_").toLowerCase().trim();
-		int startWith = 0;
-		while (startWith < temp.length()) {
-			if (temp.charAt(startWith) != '_') {
-				break;
-			}
-			startWith++;
-		}
-		return temp.substring(startWith);
+	public static String splitJoin(String str, String join) {
+		return splitJoin(str, join, DEFAULT_PARTICIPLE_EXCLUDES, DEFAULT_PARTICIPLE_TO_JOIN);
 	}
+
 	/**
 	 * 将字符串大小写字母按词拆分,并将其连接成新的字符串
 	 * 
 	 * @param str
 	 *          字符串
 	 * @param join
-	 *          连接词
+	 *          连接词,如果字符串存在join则按join分词并返回
 	 * @param excludes
 	 *          去除掉那些词
+	 * @param replaceToJoin
+	 *          将那些词替换为join,替换后如果字符串存在join则按join分词并返回
 	 * @return
 	 */
-	public static String splitJoin(String str, String join, String... excludes) {
-		for (String r : excludes) {
-			str = str.replace(r, "");
+	public static String splitJoin(String str, String join, List<String> excludes, List<String> replaceToJoin) {
+		// 删除排除的词
+		if (excludes != null) {
+			for (String r : excludes) {
+				str = str.replace(r, "");
+			}
 		}
-		//TODO 完善工具分词
-		List<String> items = new ArrayList<String>();
-		int i = 0;
-		while (i < str.length()) {
+		// 替换指定词为join
+		if (replaceToJoin != null) {
+			for (String r : replaceToJoin) {
+				str = str.replace(r, join);
+			}
+		}
+		// 替换字符串多种多个join为一个
+		str = str.replaceAll(join + "+", join);
+		// if (str.indexOf(join) != -1) {
+		// return str.toLowerCase();
+		// }
+		// 分词
+		List<String> splits = new ArrayList<String>();
+		for (int i = 0; i < str.length();) {
 			StringBuilder word = new StringBuilder();
 			char c = str.charAt(i);
 			word.append(c);
 			i++;
 			if (isLetters(c)) {
 				while (i < str.length()) {
-					char next = str.charAt(i);
-					if (isLetters(next)) {
-						if (isLower(next) != isLower(c)) {
+					char c1 = str.charAt(i);
+					if (isLetters(c1)) {
+						if (isLower(c) == isLower(c1)) {
+							word.append(c1);
+							i++;
+						} else {
 							break;
 						}
-						c = next;
-						word.append(c);
+					} else {
+						break;
+					}
+				}
+			} else {
+				while (i < str.length()) {
+					char c1 = str.charAt(i);
+					if (!isLetters(c1)) {
+						word.append(c1);
 						i++;
+					} else {
+						break;
 					}
 				}
 			}
-			items.add(word.toString());
+			splits.add(word.toString());
 		}
-		return magre(join, items).replaceAll(join + "+", join).toLowerCase();
+		// 组词
+		List<String> items = new ArrayList<String>();
+		for (int i = 0; i < splits.size();) {
+			if (splits.get(i).length() == 1) {
+				if (isNumber(splits.get(i))) {
+					if (items.size() > 0) {
+						String last = items.get(items.size() - 1);
+						items.remove(items.size() - 1);
+						items.add(last + splits.get(i));
+						i++;
+						if (i == splits.size()) {
+							break;
+						}
+					}
+				} else {
+					StringBuilder sb = new StringBuilder(splits.get(i));
+					i++;
+					if (i < splits.size()) {
+						sb.append(splits.get(i));
+						i++;
+					}
+					items.add(sb.toString());
+				}
+			} else {
+				items.add(splits.get(i));
+				i++;
+			}
+		}
+		return magre(join, items).toLowerCase().replaceAll(join + "+", join);
 	}
+
 	/**
 	 * 判断字符是否为英文中的26大小写字母
 	 * 
@@ -253,6 +297,21 @@ public class SdUtil {
 	}
 
 	/**
+	 * 判断字符串是否为数字
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isNumber(String str) {
+		for (char c : str.toCharArray()) {
+			if (!(c >= 48 && c <= 57)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
 	 * 检查字符串里面是否包含指定字符,包含返回true
 	 * 
 	 * @param regex
@@ -292,6 +351,7 @@ public class SdUtil {
 		}
 		return result == null ? null : result.toString();
 	}
+
 	/**
 	 * 将字符串集合按指定字符串拼接成一个新的字符串
 	 * 
@@ -302,14 +362,18 @@ public class SdUtil {
 	 * @return
 	 */
 	public static String magre(String separator, List<String> items) {
-		StringBuilder result = null;
+		StringBuilder result = new StringBuilder();
+		boolean first = true;
 		for (String temp : items) {
-			if (result == null) {
-				result = new StringBuilder(temp);
+			if (!first) {
+				result.append(separator);
 			}
-			result.append(separator + temp);
+			result.append(temp);
+			if (first) {
+				first = false;
+			}
 		}
-		return result == null ? null : result.toString();
+		return result.toString();
 	}
 
 	/**
--- src/test/java/org/mirrentools/sd/SdUtilTest.java
+++ src/test/java/org/mirrentools/sd/SdUtilTest.java
@@ -4,11 +4,20 @@ import org.mirrentools.sd.common.SdUtil;
 
 public class SdUtilTest {
 	public static void main(String[] args) {
-		String score = SdUtil.replaceToUnderScore("nameREMOVEnameIsMirren", " ", "-");
-		System.out.println(score);
-		System.out.println("*********************");
-		String camelCase = SdUtil.toCamelCase("NameREMOVEnameIsMirren");
+		String str = "my_name_is_mirren-my-NameIsMirren出生年份BornIn1991";
+		System.out.println("原字符串: " + str);
+		String camelCase = SdUtil.toCamelCase(str);
 		System.out.println(camelCase);
+		String pascalCase = SdUtil.toPascalCase(str);
+		System.out.println(pascalCase);
+		String underScoreCase = SdUtil.toUnderScoreCase(str);
+		System.out.println(underScoreCase);
+		String upperUnderScoreCase = SdUtil.toUnderScoreCaseUpper(str);
+		System.out.println(upperUnderScoreCase);
+		String hyphenCase = SdUtil.toHyphenCase(str);
+		System.out.println(hyphenCase);
+		String upperHyphenCase = SdUtil.toHyphenCaseUpper(str);
+		System.out.println(upperHyphenCase);
 
 	}
 }
