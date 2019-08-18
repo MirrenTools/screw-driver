02104d3abb8edfed0d2887fd70b1761f8d1beeff
likangkang <353730289@qq.com>
2019-08-18 21:00:39 +0800

    测试提交

--- src/main/java/org/mirrentools/sd/models/db/update/SdAbstractTableContent.java
+++ src/main/java/org/mirrentools/sd/models/db/update/SdAbstractTableContent.java
@@ -16,6 +16,14 @@ public abstract class SdAbstractTableContent {
 	private String tableName;
 	/** 表的注释 */
 	private String remark;
+	/** 模式名*/
+	 private String schema;
+	 /** 拥有者 */
+	 private String owner;
+	 /** 索引表空间*/
+	 private String indexspace;
+	 /** 是否压缩*/
+	 private boolean compressable;
 	/** 如果不存在就创建 */
 	private boolean ifNotExist;
 	/** 表列的属性 */
@@ -28,6 +36,8 @@ public abstract class SdAbstractTableContent {
 	private List<SdAbstractForeignKeyContent> foreignKeys;
 	/** 拓展属性 */
 	private Map<String, Object> extensions;
+	private List<String> appends;
+	
 
 	/**
 	 * 创建表的SQL语句
@@ -279,7 +289,64 @@ public abstract class SdAbstractTableContent {
 		this.extensions = extensions;
 		return this;
 	}
+	public  String getSchema() {
+		return schema;
+	}
 
+	public SdAbstractTableContent  setSchema(String schema) {
+		this.schema = schema;
+		return this;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public SdAbstractTableContent  setOwner(String owner) {
+		this.owner = owner;
+		return this;
+	}
+
+	public String getIndexspace() {
+		return indexspace;
+	}
+
+	public SdAbstractTableContent  setIndexspace(String indexspace) {
+		this.indexspace = indexspace;
+		return this;
+	}
+
+	public boolean isCompressable() {
+		return compressable;
+	}
+
+	public SdAbstractTableContent  setCompressable(boolean compressable) {
+		this.compressable = compressable;
+		return this;
+	}
+
+	public List<String> getAppends() {
+		return appends;
+	}
+
+	public SdAbstractTableContent setAppends(List<String> appends) {
+		this.appends = appends;
+		return this;
+	}
+	
+	/**
+	 * 添加外键属性
+	 * 
+	 * @param foreignKey
+	 */
+	public SdAbstractTableContent addAppends(String append) {
+		if (getAppends() == null) {
+			setAppends(new ArrayList<String>());
+		}
+		getAppends().add(append);
+		return this;
+	}
+	
 	@Override
 	public String toString() {
 		return "SdAbstractTableContent [tableName=" + tableName + ", remark=" + remark + ", colums=" + colums + ", primaryKey=" + primaryKey + ", indexKeys=" + indexKeys + ", foreignKeys=" + foreignKeys
