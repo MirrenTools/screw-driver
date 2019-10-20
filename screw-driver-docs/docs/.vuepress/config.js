40603abd30af49ff39956dfc1bb9050bc0ef22b1
Mirren <690389491@qq.com>
2019-10-20 22:48:06 +0800

    init docs

---
+++ screw-driver-docs/docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'ScrewDriver V1.1.0',
+	description: '用于生成代码、创建或读取数据库表的工具包',
+  dest:'dist',
+	themeConfig: {
+  	nav:[{ text: 'MirrenTools', link: 'https://mirrentools.org'}],
+  	sidebar:{
+  		'/intro/':[{title:'介绍', collapsable: true,children:['/intro/introduction']}]
+  		}
+    }
+}
