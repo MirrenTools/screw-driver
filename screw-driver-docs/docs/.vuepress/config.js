5bc1405b7de368484025cd52e0b921ac5db42de9
shenzhenMirren <690389491@qq.com>
2019-10-21 12:50:07 +0800

    add config setting

--- screw-driver-docs/docs/.vuepress/config.js
+++ screw-driver-docs/docs/.vuepress/config.js
@@ -3,13 +3,15 @@ module.exports = {
 	description: '用于生成代码、创建或读取数据库表的工具包',
 	dest:'dist',
 	themeConfig: {
+	displayAllHeaders: true,
+	activeHeaderLinks: false,
   	nav:[
 			{ text: 'MirrenTools', link: 'https://mirrentools.org'},
 			{ text: 'github', link: 'https://github.com/MirrenTools/screw-driver'},
 			{ text: 'gitee', link: 'https://gitee.com/duhua/screw-driver'}
 		],
   	sidebar:{
-  		'/':[{title:'起步', collapsable: true,children:['/start/introduction']}]
+  		'/':[{title:'使用说明', collapsable: false,children:['/start/introduction','/start/core-class-intro']}]
   		}
     }
 }
---
+++ screw-driver-docs/docs/start/core-class-intro.md
@@ -0,0 +1,9 @@
+# 核心类与接口介绍
+## SdBean
+实体类的描述
+## SdColumn
+实体类属性的描述
+## ScrewDriverOptions
+接口的配置文件
+## ScrewDriver
+工具的接口
